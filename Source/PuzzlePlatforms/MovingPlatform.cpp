f347ba637dcd9b53c56f212e0f1984740d1bf0e3
bradleyb <bradleyb@sfu.ca>
2021-09-30 16:12:59 -0700

    Added MovingPlatform actor and updated default map with sample puzzle

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform() {
+	// Enable tick
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Ensure component is Movable
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay() {
+	Super::BeginPlay();
+
+	if (HasAuthority()) { // Only replicate from server
+		GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+		GlobalStartLocation = GetActorLocation();
+
+		// Replicate location from server to client
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+}
+
+void AMovingPlatform::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority()) { // Only move on server
+		FVector Location = GetActorLocation();
+
+		if (FVector::Distance(GlobalStartLocation, Location) >= FVector::Distance(GlobalStartLocation, GlobalTargetLocation)) {
+			FVector tmp = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = tmp;
+		}
+		// Calculate and set new location of not at target location
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		Location += Speed * DeltaTime * Direction;
+		SetActorLocation(Location);
+	}
+
+	//bool IsServer = HasAuthority();
+}
